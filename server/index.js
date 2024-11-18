589dcca2962fdb9892949ab7f203b775087a8fbc
Estifanos58 <122863605+Estifanos58@users.noreply.github.com>
2024-11-18 22:03:25 +0300

    before

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
-const ws = require('ws');
-const server = new ws.Server({port:'3000'})
+const ws = require('ws')
+const server = new ws.Server({ port: '3000' })
 
 server.on('connection', socket => {
-    socket.on('message', message =>{
-        console.log(message)
+    socket.on('message', message => {
+        const b = Buffer.from(message)
+        console.log(b.toString())
         socket.send(`${message}`)
     })
 })
